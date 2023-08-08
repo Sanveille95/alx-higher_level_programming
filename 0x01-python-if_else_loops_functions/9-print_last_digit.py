50dc966fb79b12869db5b8019d2e9155f181f2e0
Sanveille95 <snwoleymorgan95@gmail.com>
2023-08-08 05:54:34 -0700

    fixed indentation

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 def print_last_digit(number):
     last_digit = number % 10
     if number < 0:
-            last_digit = (number * -1) % 10
+        last_digit = (number * -1) % 10
     print("{}".format(last_digit), end="")
     return last_digit
